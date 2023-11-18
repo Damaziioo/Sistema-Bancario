c72c48f894af6107adb0dea7b318e47f9a175ad1
Lucas Damazio <lucas.damaziioo@gmail.com>
2023-11-18 16:44:13 -0300

    Criação das Funções
    Nesse Commit foi feita a criação das funções básicas.

--- Sistema-Bancário.py
+++ Sistema-Bancário.py
@@ -15,47 +15,65 @@ deposito = 0.00
 saldo = 0.00
 extrato= ""
 
+def sacar(saldo = 0 ,saque_diario = 0,LIMITE_SAQUE_DIARIO=0, extrato = " " ,LIMITE_POR_SAQUE=" "):
+
+    if( saque_diario>=LIMITE_SAQUE_DIARIO):
+        return print("Você atingiu o limite de saques diários!")
+    else:
+
+        valor_saque = float(input("Insira o valor a ser sacado: "))
+
+        if valor_saque <= 0:
+            return print("Operação falhou. Não é permitido sacar valores nulos ou negativos!")
+            
+        elif valor_saque > LIMITE_POR_SAQUE:
+            return print(f"Operação falhou. Você ultrapassou o limite por saque, insira um valor abaixo de R$ {LIMITE_POR_SAQUE}!")
+            
+        elif valor_saque > saldo:
+          return  print("Operação falhou. Você não tem saldo suficiente.")
+        else:
+            print(f"\nValor de R$ {valor_saque:.2f} sacado com sucesso!")
+            saldo -= valor_saque
+            saque_diario += 1
+            extrato.join(f"Saque: R$ {valor_saque:.2f} \n")
+            return print(f"\nValor de R$ {valor_saque:.2f} sacado com sucesso!"), saldo, saque_diario, extrato
+
+
+def depositar(saldo, lista_extrato):
+
+    deposito=float(input("Insira o valor a ser depositado: "))
+
+    if deposito > 0:
+        saldo+=deposito
+        lista_extrato.join(f"Deposito: R$ {deposito:.2f} \n")
+        print(f"\n Valor de R$ {deposito:.2f} depositado com sucesso!")
+    elif deposito<=0:
+        print("Não é permitido depositar valores nulos ou negativos!")
+    else:
+        print("Operação falhou!")
+
+def emitir_extrato(saldo,extrato=" "):
+        print(" EXTRATO ".center(35,"="))
+        print("Não foram realizadas movimentações!" if not extrato else extrato)
+        print(f"Saldo: R$ {saldo:.2f}")
+        print('='*35)
+
 while True:
     opcao = input(menu).lower()
 
     if opcao=="d": #Deposito
         
-        deposito=float(input("Insira o valor a ser depositado: "))
-        if deposito > 0:
-            saldo+=deposito
-            extrato += f"Deposito: R$ {deposito:.2f} \n"
-            print(f"\n Valor de R$ {deposito:.2f} depositado com sucesso!")
-        else:
-            print("Não é permitido depositar valores nulos ou negativos!")
+        depositar(saldo, extrato)
                 
     elif opcao == "s": #Saque
-        if( saque_diario>=LIMITE_SAQUE_DIARIO):
-            print("Você atingiu o limite de saques diários!")
-
-        saque = float(input("Insira o valor a ser sacado: "))
-
-        if saque <= 0:
-            print("Operação falhou. Não é permitido sacar valores nulos ou negativos!")
-            
-        elif saque > LIMITE_POR_SAQUE:
-            print(f"Operação falhou. Você ultrapassou o limite por saque, insira um valor abaxo de R$ {LIMITE_POR_SAQUE}!")
-            
-        elif saque > saldo:
-            print("Operação falhou. Você não tem saldo suficiente.")
-        else:
-            print("Operação falou. Valor informado é inválido.")
-
-        print(f"\nValor de R$ {saque:.2f} sacado com sucesso!")
-        saldo -= saque
-        saque_diario += 1
-        extrato += f"Saque: R$ {saque:.2f} \n"
+        sacar(saldo,saque_diario, LIMITE_SAQUE_DIARIO, extrato )
 
     elif opcao == "e": #Extrato
-        print(" EXTRATO ".center(35,"="))
-        print("Não foram realizadas movimentações!" if not extrato else extrato)
-        print(f"Saldo: R$ {saldo:.2f}")
-        print('='*35)
+
+        emitir_extrato(saldo,extrato=extrato)
+
     elif opcao == "q":
+
         break
 
     else:
