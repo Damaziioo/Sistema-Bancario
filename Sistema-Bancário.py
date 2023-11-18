e059fcbc98c66a2a8324998fecfc601b1d8275d8
Lucas Damazio <lucas.damaziioo@gmail.com>
2023-11-18 15:48:23 -0300

    Ajuste no Saque

--- Sistema-Bancário.py
+++ Sistema-Bancário.py
@@ -31,30 +31,31 @@ while True:
     elif opcao == "s": #Saque
         if( saque_diario>=LIMITE_SAQUE_DIARIO):
             print("Você atingiu o limite de saques diários!")
-
-        saque = float(input("Insira o valor a ser sacado: "))
-
-        if saque <= 0:
-            print("Operação falhou. Não é permitido sacar valores nulos ou negativos!")
-            
-        elif saque > LIMITE_POR_SAQUE:
-            print(f"Operação falhou. Você ultrapassou o limite por saque, insira um valor abaxo de R$ {LIMITE_POR_SAQUE}!")
-            
-        elif saque > saldo:
-            print("Operação falhou. Você não tem saldo suficiente.")
         else:
-            print("Operação falou. Valor informado é inválido.")
 
-        print(f"\nValor de R$ {saque:.2f} sacado com sucesso!")
-        saldo -= saque
-        saque_diario += 1
-        extrato += f"Saque: R$ {saque:.2f} \n"
+            saque = float(input("Insira o valor a ser sacado: "))
+
+            if saque <= 0:
+                print("Operação falhou. Não é permitido sacar valores nulos ou negativos!")
+            
+            elif saque > LIMITE_POR_SAQUE:
+                print(f"Operação falhou. Você ultrapassou o limite por saque, insira um valor abaxo de R$ {LIMITE_POR_SAQUE}!")
+            
+            elif saque > saldo:
+                print("Operação falhou. Você não tem saldo suficiente.")
+            else:
+                print(f"\nValor de R$ {saque:.2f} sacado com sucesso!")
+                saldo -= saque
+                saque_diario += 1
+                extrato += f"Saque: R$ {saque:.2f} \n"
 
     elif opcao == "e": #Extrato
+
         print(" EXTRATO ".center(35,"="))
         print("Não foram realizadas movimentações!" if not extrato else extrato)
         print(f"Saldo: R$ {saldo:.2f}")
         print('='*35)
+        
     elif opcao == "q":
         break
 
